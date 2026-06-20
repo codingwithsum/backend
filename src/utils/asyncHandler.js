5a8fbc75a508e97e12be5ce818c7bee8aaca27f0
Sumit Manani <162180463+codingwithsum@users.noreply.github.com>
2026-06-20 22:05:23 +0530

    utils code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,16 @@
+const asyscHandler = (requestHandler) => {
+     (req, res, next) => {
+        promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }   
+}
+
+export { asyscHandler }
+
+// const asyscHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(error.code || 500).json({success: false, message: error.message })
+//     }
+
+// }
